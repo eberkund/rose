772a0ec4298f544be014cac6d0362e9f5c73a9b1
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-05-21 19:20:16 -0400

    Fix formatter

--- gold/gold_test.go
+++ gold/gold_test.go
@@ -108,7 +108,6 @@ func TestUpdate(t *testing.T) {
 }
 
 func TestFailingDiff(t *testing.T) {
-
 	tm := mocks.NewTesting(t)
 	tm.On("Fatalf", mock.Anything, mock.Anything).Once()
 	g := gold.New(tm, gold.WithPrefix("testdata", "TestExistingFiles", "json"))
