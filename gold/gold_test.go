198b7cea360171bcc587bf121172a53ab51cb51f
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-06-01 11:53:59 -0400

    Cleanup

--- gold/gold_test.go
+++ gold/gold_test.go
@@ -23,22 +23,22 @@ func TestExistingFiles(t *testing.T) {
 		"json": {
 			inputData:  `{"foo":123,"bar":"hello world","a":true}`,
 			goldenFile: "json_eq.golden.json",
-			testFn: func(gold *gold.Gold) func(string, string) {
-				return gold.JSONEq
+			testFn: func(g *gold.Gold) func(string, string) {
+				return g.JSONEq
 			},
 		},
 		"text": {
 			goldenFile: "text_eq.golden.txt",
 			inputData:  "Hello\nWorld\n!",
-			testFn: func(gold *gold.Gold) func(string, string) {
-				return gold.Eq
+			testFn: func(g *gold.Gold) func(string, string) {
+				return g.Eq
 			},
 		},
 		"html": {
 			goldenFile: "xml_eq.golden.toml",
 			inputData:  `<fruits><apple/><banana/></fruits>`,
-			testFn: func(gold *gold.Gold) func(string, string) {
-				return gold.HTMLEq
+			testFn: func(g *gold.Gold) func(string, string) {
+				return g.HTMLEq
 			},
 		},
 		"toml": {
@@ -51,8 +51,8 @@ Pi = 3.14
 Perfection = [ 6, 28, 496, 8128 ]
 DOB = 1987-07-05T05:45:00Z
 `,
-			testFn: func(gold *gold.Gold) func(string, string) {
-				return gold.TOMLEq
+			testFn: func(g *gold.Gold) func(string, string) {
+				return g.TOMLEq
 			},
 		},
 		"yaml": {
@@ -74,12 +74,12 @@ jobs:
 	}
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			gold := gold.New(
+			g := gold.New(
 				t,
 				gold.WithPrefix("testdata", t.Name()),
 				gold.WithFlag(*update),
 			)
-			f := tc.testFn(gold)
+			f := tc.testFn(g)
 			f(tc.goldenFile, tc.inputData)
 		})
 	}
