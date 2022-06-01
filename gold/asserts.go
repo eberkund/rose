4114623792cafd04e9fe634e6c34d0b71588c751
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-06-01 11:53:59 -0400

    Update helper

--- gold/asserts.go
+++ gold/asserts.go
@@ -6,25 +6,30 @@ import (
 
 // JSONEq compares XML to golden file.
 func (g *Gold) JSONEq(goldenPath, actual string) {
+	g.t.Helper()
 	g.fail(g.assert(goldenPath, actual, formatting.JSON))
 }
 
 // HTMLEq compares XML to golden file.
 func (g *Gold) HTMLEq(goldenPath, actual string) {
+	g.t.Helper()
 	g.fail(g.assert(goldenPath, actual, formatting.HTML))
 }
 
 // TOMLEq compares TOML to golden file.
 func (g *Gold) TOMLEq(goldenPath, actual string) {
+	g.t.Helper()
 	g.fail(g.assert(goldenPath, actual, formatting.TOML))
 }
 
 // YAMLEq compares YAML to golden file.
 func (g *Gold) YAMLEq(goldenPath, actual string) {
+	g.t.Helper()
 	g.fail(g.assert(goldenPath, actual, formatting.YAML))
 }
 
 // Eq compares string to golden file.
 func (g *Gold) Eq(goldenPath, actual string) {
+	g.t.Helper()
 	g.fail(g.assert(goldenPath, actual, formatting.NoOp))
 }
