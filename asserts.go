d37d2101f224b83c1296e4b41c523e6b065bc50e
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-06-01 11:53:57 -0400

    Cleanup

--- asserts.go
+++ asserts.go
@@ -2,25 +2,25 @@ package rose
 
 // JSONEq compares XML to golden file.
 func (g *Gold) JSONEq(goldenPath, actual string) {
-	g.genericEQ(g.withPrefix(goldenPath), actual, formatJSON)
+	g.genericEQ(goldenPath, actual, formatJSON)
 }
 
 // XMLEq compares XML to golden file.
 func (g *Gold) XMLEq(goldenPath, actual string) {
-	g.genericEQ(g.withPrefix(goldenPath), actual, formatXML)
+	g.genericEQ(goldenPath, actual, formatXML)
 }
 
 // TOMLEq compares TOML to golden file.
 func (g *Gold) TOMLEq(goldenPath, actual string) {
-	g.genericEQ(g.withPrefix(goldenPath), actual, formatTOML)
+	g.genericEQ(goldenPath, actual, formatTOML)
 }
 
 // YAMLEq compares YAML to golden file.
 func (g *Gold) YAMLEq(goldenPath, actual string) {
-	g.genericEQ(g.withPrefix(goldenPath), actual, formatYAML)
+	g.genericEQ(goldenPath, actual, formatYAML)
 }
 
 // Eq compares string to golden file.
 func (g *Gold) Eq(goldenPath, actual string) {
-	g.genericEQ(g.withPrefix(goldenPath), actual, formatNoop)
+	g.genericEQ(goldenPath, actual, formatNoop)
 }
